8d3b969ec3ecbbb01a80b929012c5ec2113dad83
BrunoSoares21 <brunosoaresdasilva25@gmail.com>
2024-08-23 21:59:16 -0300

    Create new database

--- src/controllers/task/task.controller.ts
+++ src/controllers/task/task.controller.ts
@@ -4,21 +4,32 @@ import Task from '../../models/task.entity'
 export default class TaskController{
 
     static async index(req: Request, res: Response){
-        const tasks = await Task.find()
+        const tasks = await Task.find({
+            select: {
+                user: {
+                    id: true,
+                    name: true
+                }
+            },
+            relations: {
+                user: true
+            }
+        })
         return res.json(tasks)
     }
 
     static async store(req: Request, res: Response){
-        const {title, completed} = req.body
+        const {title, completed, userId} = req.body
 
-        if(!title){
-            return res.status(400).json({msg: "Erro"})
+        if(!title || !userId){
+            return res.status(400).json({msg: "Título/user obrigatório"})
         }
 
         const task = new Task()
         task.title = title
         task.completed = completed
-        task.save()
+        task.userId = userId
+        await task.save()
 
         return res.status(201).json(task)
     }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from "express"
 import taskRoutes from './task/task.routes'
-import { deflateSync } from "zlib"
+import userRoutes from "./user/user.routes"
 
 const routes = Router()
 
 routes.use('/task', taskRoutes)
+routes.use('/user', userRoutes)
 
 export default routes
