d16b9f1fef1ac1dce5eb3ba160fbd7b13ff6e242
BrunoSoares21 <brunosoaresdasilva25@gmail.com>
2024-09-07 19:02:32 -0300

    Comments

--- src/middlewares/auth.middlewares.ts
+++ src/middlewares/auth.middlewares.ts
@@ -7,13 +7,18 @@ export default async function authMiddleware(req: Request, res: Response, next: 
     if(!authorization) return res.status(401).json({msg: 'Token inválido'})
     
     const userToken = await Token.findOneBy({token: authorization})
+    
     if(!userToken) return res.status(401).json({msg: 'Token inválido'})
     
+    // Verifica se o token expirou
     if(userToken.expiresAt < new Date()){
         userToken.remove()
         return res.status(401).json({msg: 'Token expirado'})
     }
 
+    // Adiciona o id do usuário no header da requisição
     req.headers.userId = userToken.userId.toString()
+
+    // Continua a execução
     next()
 }
